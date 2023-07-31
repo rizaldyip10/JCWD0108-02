2d671d2c3b5c001ef86fbb7525d0182e3918c389
faridsuryadi <132431667+faridsuryadi@users.noreply.github.com>
2023-07-31 13:18:56 +0700

    admin-dashboard (#7)
    * landing page 1
    
    * Update App.js
    
    * Squashed commit of the following:
    
    commit d48889f9fa32dc33168fc90ea8bcc022b60bfce8
    Author: faridsuryadi <faridsuryadi27@gmail.com>
    Date:   Mon Jul 31 10:20:34 2023 +0700
    
        admin dsahboard
    
        tes anjay

---
+++ projects/client/src/components/admin/editModal.jsx
@@ -0,0 +1,30 @@
+// ProductModal.js
+import React from 'react';
+import {
+  Modal, ModalOverlay, ModalContent, ModalHeader,
+  ModalCloseButton, ModalBody, ModalFooter, Button
+} from '@chakra-ui/react';
+import { EditProduct } from './editProduct';
+
+export const EditModal = ({ isOpen, onClose, selectedProduct, onSave, categories }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Edit Product</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {selectedProduct && (
+            <EditProduct product={selectedProduct} onSave={onSave} categories={categories} />
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
